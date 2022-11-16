8c407e3db00043ecb5b81424648a2fe4e54b480d
Douglas <douglas-cprdev@hotmail.com>
2022-11-16 08:42:43 -0300

    adding singleton pattern example

---
+++ creational/Singleton/Main.cs
@@ -0,0 +1,35 @@
+﻿namespace Singleton;
+
+public class BankAccount 
+{
+  private BankAccount _instance;
+  private readonly object _lock = new object();
+
+  private constructor()
+  {
+    _instance = new BankAccount();
+  }
+
+  public static BankAccount GetInstance()
+  {
+    if (_instance is null)
+    {
+      lock(_lock) // block other threads
+      {
+        _instance = new BankAccount();
+      }
+    }
+
+    return _instance;
+  }
+}
+
+
+public class Main
+{
+  public void App()
+  {
+    var account = BankAccount.GetInstance(); 
+    var cachedAccount = BankAccount.GetInstance();
+  }
+}
